a1237cff25e0ed69723633aab36df4716412c21c
kasat <kasat@DESKTOP-DOQKB49>
2022-11-13 23:52:00 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PrototypeFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            IFigure figure = new Rectangle(10, 20);
+            IFigure clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            figure = new Circle(15);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            figure = new Triangle(3,4,5);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            Console.Read();
+        }
+    }
+    interface IFigure
+    {
+        IFigure Clone();
+        void GetInfo();
+    }
+    class Rectangle : IFigure
+    {
+        int width;
+        int height;
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public IFigure Clone()
+        {
+            return new Rectangle(this.width, this.height);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Прямокутник довжиною {0} и шириною{1} ", height, width);
+        }
+    }
+    class Circle : IFigure
+    {
+        int radius;
+        public Circle(int r)
+        {
+            radius = r;
+        }
+        public IFigure Clone()
+        {
+            return new Circle(this.radius);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Круг радіусом {0}", radius);
+        }
+    }
+    class Triangle : IFigure
+    {
+        int firstSide;
+        int secondSide;
+        int thirdSide;
+        public Triangle(int _firstSide, int _secondSide, int _thirdSide)
+        {
+            this.firstSide = _firstSide;
+            this.secondSide = _secondSide;
+            this.thirdSide = _thirdSide;
+        }
+        public IFigure Clone()
+        {
+            return new Triangle(this.firstSide, this.secondSide, this.thirdSide);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Трикутник із сторонами {0}, {1}, {2}", firstSide, secondSide, thirdSide);
+        }
+    }
+    
+}
